5eb782872c8c3c08afa853cce9e95c9a6c921bdf
Noura Elarabi <NElarabi@integrant.com>
2019-09-07 21:13:27 +0300

    initial project

---
+++ src/RealEstate/App_Start/RealEstateContext.cs
@@ -0,0 +1,40 @@
+﻿namespace RealEstate.App_Start
+{
+	using MongoDB.Driver;
+	using Properties;
+	using Rentals;
+
+	public class RealEstateContext
+	{
+		public MongoDatabase Database;
+
+		public RealEstateContext()
+		{
+			var client = new MongoClient(Settings.Default.RealEstateConnectionString);
+			var server = client.GetServer();
+			Database = server.GetDatabase(Settings.Default.RealEstateDatabaseName);
+		}
+
+		public MongoCollection<Rental> Rentals => Database.GetCollection<Rental>("rentals");
+	}
+
+	public class RealEstateContextNewApis
+	{
+		public IMongoDatabase Database;
+
+		public RealEstateContextNewApis()
+		{
+			var client = new MongoClient(Settings.Default.RealEstateConnectionString);
+			Database = client.GetDatabase(Settings.Default.RealEstateDatabaseName);
+		}
+
+		public IMongoCollection<Rental> Rentals => Database.GetCollection<Rental>("rentals");
+	}
+}
+
+
+
+
+
+
+
---
+++ src/RealEstate/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Driver;
+using RealEstate.App_Start;
+using RealEstate.Properties;
+
+namespace RealEstate.Controllers
+{
+	using System.Threading.Tasks;
+	using MongoDB.Bson;
+
+	public class HomeController : Controller
+	{
+		public static RealEstateContextNewApis Context = new RealEstateContextNewApis();
+
+		public async Task<ActionResult> Index()
+		{
+			var buildInfoCommand = new BsonDocument("buildinfo", 1);
+			var buildInfo = await Context.Database.RunCommandAsync<BsonDocument>(buildInfoCommand);
+			return Content(buildInfo.ToJson(), "application/json");
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
