96ead029468018b81567e7c50cf90413cc92c2b1
Noura Elarabi <NElarabi@integrant.com>
2019-10-19 19:05:12 +0300

    Module 3 : Diagnosing Database Interactions

--- src/RealEstate/App_Start/RealEstateContext.cs
+++ src/RealEstate/App_Start/RealEstateContext.cs
@@ -1,10 +1,13 @@
 ﻿namespace RealEstate.App_Start
 {
-	using MongoDB.Driver;
-	using Properties;
-	using Rentals;
+    using System;
+    using log4net;
+    using MongoDB.Driver;
+    using MongoDB.Driver.Core.Events;
+    using Properties;
+    using Rentals;
 
-	public class RealEstateContext
+    public class RealEstateContext
 	{
 		public MongoDatabase Database;
 
@@ -24,12 +27,61 @@
 
 		public RealEstateContextNewApis()
 		{
-			var client = new MongoClient(Settings.Default.RealEstateConnectionString);
-			Database = client.GetDatabase(Settings.Default.RealEstateDatabaseName);
+            var connectionString = Settings.Default.RealEstateConnectionString;
+            var settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+            //settings.ClusterConfigurator = builder => builder.Subscribe<CommandStartedEvent>(started =>
+            //{
+            //    //do something
+            //});
+            settings.ClusterConfigurator = builder => builder.Subscribe(new logMongoEvents());
+            var client = new MongoClient(settings);
+
+            //var client = new MongoClient(Settings.Default.RealEstateConnectionString);
+            Database = client.GetDatabase(Settings.Default.RealEstateDatabaseName);
 		}
 
 		public IMongoCollection<Rental> Rentals => Database.GetCollection<Rental>("rentals");
 	}
+
+    public class logMongoEvents: IEventSubscriber
+    {
+        private static ILog CommandStartedLog = LogManager.GetLogger("CommandStarted");
+        private ReflectionEventSubscriber _Subscriber;
+        public logMongoEvents()
+        {
+            _Subscriber = new ReflectionEventSubscriber(this);
+        }
+
+        public bool TryGetEventHandler<TEvent>(out Action<TEvent> handler)
+        {
+            //if (typeof(TEvent) != typeof(CommandStartedEvent))
+            //{
+            //    handler = null;
+            //    return false;
+            //}
+
+            //handler = e =>
+            //{
+
+            //};
+            //return true;
+
+            return _Subscriber.TryGetEventHandler(out handler); // looks for a function called Handle that takes the type of event and returns void
+        }
+
+        public void Handle(CommandStartedEvent started){
+            CommandStartedLog.Info(new
+            {
+                started.Command,
+                started.CommandName
+            });
+        }
+
+        public void Handle(CommandSucceededEvent succeeded)
+        {
+
+        }
+    }
 }
 
 
